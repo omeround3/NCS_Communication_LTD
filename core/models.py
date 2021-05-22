5511d41bbd1cea61af63c220e40b72a4903aa486
Tal-Nitzan <tal.nitzan91@gmail.com>
2021-05-22 14:47:12 +0300

    Added Clients table to DB. Fixed Clients preview in HTML (Dashboard, Clients). Added 'AddClient' Module to Clients page.

--- core/models.py
+++ core/models.py
@@ -2,17 +2,21 @@ from django.db import models
 
 # Create your models here.
 
+
 class Client(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     email = models.EmailField(max_length=254)
-    cellphone = models.IntegerField()
+    cellphone = models.CharField(max_length=15)
     SUBSCRIPTION = [
         ('150', '150 Mbps'),
         ('500', '500 Mbps'),
         ('1000', '1 Gbps'),
     ]
-    bandwidth = models.CharField(max_length=4, choices=SUBSCRIPTION, default='500')
+    bandwidth = models.CharField(
+        max_length=4, choices=SUBSCRIPTION, default='500')
     start_date = models.DateField(auto_now_add=True)
     cost = models.IntegerField(default=150)
 
+    def __str__(self):
+        return "{} {}".format(self.first_name, self.last_name)
